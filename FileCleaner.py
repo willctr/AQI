02043a5a8f384e3d03aca1e172b8ef72af78903e
Will Carter <willctr27@gmail.com>
2024-06-20 18:23:13 -0400

    cleaner functionality

--- FileCleaner.py
+++ FileCleaner.py
@@ -1,1 +1,9 @@
+import os
 import pandas as pd 
+
+# function to clean temp CSV files
+def clean_temp_files(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            filepath = os.path.join(directory, filename)
+            #year = filename.split('_')
